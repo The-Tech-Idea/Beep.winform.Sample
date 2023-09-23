c862b298e894ae6fa266281cb5c109e732bb31be
fahad@thetechidea.com <fahad@thetechidea.com>
2023-09-23 20:30:09 +0300

    update

--- Beep.Winform.Vis/MainForms/Frm_Main.cs
+++ Beep.Winform.Vis/MainForms/Frm_Main.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Windows.Forms;
+﻿using System.Data;
 using Beep.Winform.Vis.Controls;
 using TheTechIdea;
 using TheTechIdea.Beep;
