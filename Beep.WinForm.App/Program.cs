0ca02d7d4b221478fa30804a159b5f0267d03a5a
fahad@thetechidea.com <fahad@thetechidea.com>
2023-11-10 17:45:43 +0300

    update

--- Beep.WinForm.App/Program.cs
+++ Beep.WinForm.App/Program.cs
@@ -1,5 +1,12 @@
 ﻿
+using BeepEnterprize.Vis.Module;
 using BeepEnterprize.Winform;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TheTechIdea.Beep.Container;
+using TheTechIdea.Beep.Container.Services;
+using TheTechIdea.Beep.Winform.Extensions;
+using TheTechIdea.Util;
 
 namespace Beep.Winform.App
 {
@@ -15,10 +22,55 @@ namespace Beep.Winform.App
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //  Application.Run(new Form1());
-           
-            MainApp m = new MainApp();
-            
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+            builder.Services.RegisterBeep(AppContext.BaseDirectory, null, TheTechIdea.Util.BeepConfigType.Application);
+            builder.Services.RegisterVisManager();
+            // Register Other Services here
+
+            using IHost host = builder.Build();
+
+            // Retreiving Services
+            ServiceHelper.Initialize(host.Services);
+            IBeepService beepService = host.Services.GetService<IBeepService>()!;
+            IVisManager visManager = host.Services.GetService<IVisManager>()!;
+
+            //Setting the Main Form 
+            visManager.SetMainDisplay("Frm_Main", "Beep - The Data Plaform", "SimpleODM.ico", "","","");
+
+            // Adding Required Configurations
+            beepService.AddAllDataSourceMappings();
+            beepService.AddAllConnectionConfigurations();
+            beepService.AddAllDataSourceQueryConfigurations();
+
+
+            // Load Assemblies from folders (DataSources,Drivers, Extensions,...)
+            visManager.LoadAssemblies(beepService); //loading DLL using VisManager to show waiting form
+                                                    // you can also load DLL using
+                                                    // beepService.LoadAssemblies();
+                                                    //but this will not show any waiting form
+
+            // have to fo this , to work as crossplaform and Different UI
+            visManager.SetBeepReference(beepService);
+
+
+            // Load extra grahics files and icons
+            visManager.visHelper.GetGraphicFilesLocations(null, true);
+            visManager.visHelper.GetGraphicFilesLocations(beepService.DMEEditor.ConfigEditor.Config.Folders.Where(x => x.FolderFilesType == FolderFileTypes.GFX).FirstOrDefault().FolderPath, false);
+
+            // Here you can
+            // Initialize Your Services and Database Connections
+            //IDhubMainConfig dhubMainConfig = host.Services.GetService<IDhubMainConfig>()!;
+            //dhubMainConfig.InitService(false, false);
+
+
+            // Show main Page
+            visManager.ShowMainPage();
+
+            // Dispose all objects after closing application
+            beepService.vis.Dispose();
+            beepService.DMEEditor.Dispose();
+            beepService = null;
 
         }
     }
