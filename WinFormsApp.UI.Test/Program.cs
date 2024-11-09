4906898533bc9d2e79d7f219db170ef377a0190c
fahad@thetechidea.com <fahad@thetechidea.com>
2024-11-09 17:30:16 +0300

    update

--- WinFormsApp.UI.Test/Program.cs
+++ WinFormsApp.UI.Test/Program.cs
@@ -14,7 +14,7 @@ namespace WinFormsApp.UI.Test
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            var x = new BeepiForm();
+            var x = new Form1();
             Application.Run(x);
 
         }
