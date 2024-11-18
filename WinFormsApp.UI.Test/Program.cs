2b302f617f55161a4e572ce9828e119d37bd32b6
fahad@thetechidea.com <fahad@thetechidea.com>
2024-11-18 20:35:00 +0300

    updated

--- WinFormsApp.UI.Test/Program.cs
+++ WinFormsApp.UI.Test/Program.cs
@@ -14,7 +14,7 @@ namespace WinFormsApp.UI.Test
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            var x = new Form1();
+            var x = new Form2();
             Application.Run(x);
 
         }
