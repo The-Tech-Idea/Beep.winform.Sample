4ce61d2ddefa7588639046d4793f3d8b7a926caa
fahad aldhubaib <fahad@thetechidea.com>
2025-01-27 10:22:35 +0300

    fix cascade popupform and menu

--- WinFormsApp.UI.Test/Program.cs
+++ WinFormsApp.UI.Test/Program.cs
@@ -91,7 +91,7 @@ namespace WinFormsApp.UI.Test
             // Configure AppManager
             var appManager = container.Resolve<TheTechIdea.Beep.Vis.Modules.IAppManager>();
             appManager.Title = "Beep Data Management Platform";
-            appManager.Theme = EnumBeepThemes.DarkTheme;
+            appManager.Theme = EnumBeepThemes.DesertTheme;
             appManager.WaitFormType = typeof(BeepWait);
             appManager.DMEEditor.ConfigEditor.Config.SystemEntryFormName = "Form1";
             appManager.IconUrl = "simpleinfoapps.ico";
