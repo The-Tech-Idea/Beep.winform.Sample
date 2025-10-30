7776b9eb44d9e0d8bb55c67e09f3951cf4a4d3a2
fahad@thetechidea.com <fahad@thetechidea.com>
2025-10-30 07:03:14 +0300

    update

--- BenchmarkSuite1/Program.cs
+++ BenchmarkSuite1/Program.cs
@@ -7,14 +7,7 @@ namespace BenchmarkSuite1
     {
         static void Main(string[] args)
         {
-            // Run TreesRenderBenchmark when provided as argument
-            if (args.Length > 0 && args[0].IndexOf("TreesRenderBenchmark", StringComparison.OrdinalIgnoreCase) >= 0)
-            {
-                BenchmarkRunner.Run<TreesRenderBenchmark>();
-                return;
-            }
-
-            var summary = BenchmarkRunner.Run<BeepImageBenchmark>();
+           
         }
     }
 }
