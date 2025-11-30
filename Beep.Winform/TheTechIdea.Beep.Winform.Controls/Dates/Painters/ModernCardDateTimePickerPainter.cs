963b302a8e05693a4faf934693c10b268508c39c
fahad@thetechidea.com <fahad@thetechidea.com>
2025-11-30 21:17:14 +0300

    update

--- Beep.Winform/TheTechIdea.Beep.Winform.Controls/Dates/Painters/ModernCardDateTimePickerPainter.cs
+++
@@ -1,9 +0,0 @@
-        public void PaintQuickSelectionButtons(Graphics g, Rectangle buttonAreaBounds, DateTimePickerProperties properties, DateTimePickerHoverState hoverState)
-        {
-            var buttons = new[]
-            {
-                (properties.TodayButtonText, DateTime.Today),
-                (properties.TomorrowButtonText, DateTime.Today.AddDays(1)),
-                ("Next Week", DateTime.Today.AddDays(7)),
-                ("Next Month", DateTime.Today.AddMonths(1))
-            };
